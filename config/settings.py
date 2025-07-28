bf9e9184e67b2295f3667b546a777ba97a18b68b
Carlos Roberto <carlosjr.if@gmail.com>
2025-07-28 01:27:07 -0300

    fix: move STOCK_MICROSERVICE_X_API_KEY to settings.py

--- config/settings.py
+++ config/settings.py
@@ -13,3 +13,5 @@ SERVER_HOST = os.getenv("SERVER_HOST", "localhost")
 SERVER_PORT = int(os.getenv("APP_PORT", 5000))
 
 LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+
+STOCK_MICROSERVICE_X_API_KEY = os.getenv("STOCK_MICROSERVICE_X_API_KEY")
